ae2e1ddf505274a5f6477ac896876eefd2fd1bd0
Dan Silk <dan@silkster.com>
2013-07-08 00:38:21 -0400

    Updated plugin
    updated comments throughout and removed extra space characters.

--- src/jquery.texttruncater.js
+++ src/jquery.texttruncater.js
@@ -10,24 +10,24 @@
  *        The maximum height will be determined by the line height of the text within the container.  For example, to
  *        find the truncation point within a container so that only 3 lines of text will show when the line-height is
  *        18px, then the max-height of the container will be set to 3 x 18 or 54px.
- *        
+ *
  **/
 ;(function ( $, window, document, undefined ) {
-    
-    // Create the defaults once
+    // set the plugin name and the default options
     var pluginName = 'textTruncator',
         defaults = {
             maxLines: 2,
             textSelector: ''
         };
 
-    // The actual plugin constructor
+    /**
+     *  The plugin constructor method
+     **/
     function Plugin( element, options ) {
         this.element = element;
         this.options = $.extend( {}, defaults, options) ;
         this._defaults = defaults;
         this._name = pluginName;
-        
         var container = $(element).height('auto');
         this.container = {
             element: container,
@@ -37,7 +37,6 @@
         this.text = {
             selector: options.textSelector || ''
         };
-       
         this.init();
     }
 
@@ -55,6 +54,9 @@
         }
     };
 
+    /**
+     *  Initializes the plugin instance
+     **/
     Plugin.prototype.init = function () {
         var plugin = this;
         // set the text properties of the plugin instance
@@ -66,22 +68,19 @@
             plugin.text.value = $.trim(plugin.text.element.text());
         }
 
-        
         // process the text for truncation
         var words = this.text.value.split(' '),
             spanned = '<span style="display:inline-block">' + words.join('</span> <span>') + '</span>',
             ellipsis = $('<i class="dotdotdot">&#133;</i>'),
             lastWord, dotWidth, spaceWidth;
-        
         plugin.text.element.html(spanned).append(ellipsis);
         plugin._setMaxHeight();
-        
+
         // process the text and insert an ellipsis where it should be truncated
         if (plugin.container.maxHeight && plugin.text.element.height() > plugin.container.maxHeight) {
             plugin.text.element.find('span').each(function () {
                 var word = $(this),
                     pos = word.position();
-                
                 if (word.width() > plugin.container.width) {
                     word.siblings('span').hide();
                     ellipsis.css({
@@ -102,21 +101,20 @@
                         lastWord = word;
                     }
                     return false;
-                } 
+                }
                 lastWord = word;
             });
             lastWord.before(ellipsis);
         }
     };
-    
+
     // add the plugin to jQuery
     $.fn[pluginName] = function ( options ) {
         return this.each(function () {
             if (!$.data(this, 'plugin_' + pluginName)) {
-                $.data(this, 'plugin_' + pluginName, 
+                $.data(this, 'plugin_' + pluginName,
                 new Plugin( this, options ));
             }
         });
     }
-
 })( jQuery, window, document );
