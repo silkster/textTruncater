90638473d2b93ac4e78363f9c9e487ecd682ac84
Dan Silk <dan@silkster.com>
2013-07-08 01:08:58 -0400

    Plugin update
    Fixed issue when maxLines is 1

--- src/jquery.texttruncater.js
+++ src/jquery.texttruncater.js
@@ -13,21 +13,21 @@
  *
  **/
 ;(function ( $, window, document, undefined ) {
-    // set the plugin name and the default options
+    
+    // Create the defaults once
     var pluginName = 'textTruncator',
         defaults = {
             maxLines: 2,
             textSelector: ''
         };
 
-    /**
-     *  The plugin constructor method
-     **/
+    // The actual plugin constructor
     function Plugin( element, options ) {
         this.element = element;
         this.options = $.extend( {}, defaults, options) ;
         this._defaults = defaults;
         this._name = pluginName;
+        
         var container = $(element).height('auto');
         this.container = {
             element: container,
@@ -37,6 +37,7 @@
         this.text = {
             selector: options.textSelector || ''
         };
+       
         this.init();
     }
 
@@ -54,9 +55,6 @@
         }
     };
 
-    /**
-     *  Initializes the plugin instance
-     **/
     Plugin.prototype.init = function () {
         var plugin = this;
         // set the text properties of the plugin instance
@@ -73,14 +71,16 @@
             spanned = '<span style="display:inline-block">' + words.join('</span> <span style="display:inline-block">') + '</span>',
             ellipsis = $('<i class="dotdotdot">&#133;</i>'),
             lastWord, dotWidth, spaceWidth;
+        
         plugin.text.element.html(spanned).append(ellipsis);
         plugin._setMaxHeight();
-
+        
         // process the text and insert an ellipsis where it should be truncated
         if (plugin.container.maxHeight && plugin.text.element.height() > plugin.container.maxHeight) {
             plugin.text.element.find('span').each(function () {
                 var word = $(this),
                     pos = word.position();
+                
                 if (word.width() > plugin.container.width) {
                     word.siblings('span').hide();
                     ellipsis.css({
@@ -95,26 +95,27 @@
                 if (!spaceWidth && lastWord && lastWord.position().top == pos.top) {
                     spaceWidth = Math.ceil(pos.left - lastWord.width());
                 }
-                if (pos.top + word.height() >= plugin.container.maxHeight) {
+                if (lastWord && pos.top + word.height() > plugin.container.maxHeight) {
                     var leftPos = Math.ceil(lastWord.position().left) + lastWord.width() + (spaceWidth || 0);
                     if (plugin.container.width - leftPos > Math.ceil(ellipsis.width())) {
                         lastWord = word;
                     }
                     return false;
-                }
+                } 
                 lastWord = word;
             });
             lastWord.before(ellipsis);
         }
     };
-
+    
     // add the plugin to jQuery
     $.fn[pluginName] = function ( options ) {
         return this.each(function () {
             if (!$.data(this, 'plugin_' + pluginName)) {
-                $.data(this, 'plugin_' + pluginName,
+                $.data(this, 'plugin_' + pluginName, 
                 new Plugin( this, options ));
             }
         });
     }
+
 })( jQuery, window, document );
