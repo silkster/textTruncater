8b4ecc766fcac729910b2303efec2fd9a8889d24
Dan Silk <dan@silkster.com>
2013-07-08 11:07:16 -0400

    Plugin update
    refined clean-up process

--- src/jquery.texttruncater.js
+++ src/jquery.texttruncater.js
@@ -11,14 +11,25 @@
  *        find the truncation point within a container so that only 3 lines of text will show when the line-height is
  *        18px, then the max-height of the container will be set to 3 x 18 or 54px.
  *
+ *  Configurable Options:
+ *
+ *    maxLines          integer             default value is 1
+ *
+ *    textSelector      string              the element selector within the container with text that should be truncated
+ *
+ *    attributes        array or string     array: of attributes to set as the original text; examples: title or some 
+ *                                                 data- attribute like data-text
+ *                                          string: the only attribute to set
+ *
+ *
  **/
 ;(function ( $, window, document, undefined ) {
-    
     // Create the defaults once
     var pluginName = 'textTruncator',
         defaults = {
-            maxLines: 2,
-            textSelector: ''
+            maxLines: 1,
+            textSelector: '',
+            attributes: []
         };
 
     // The actual plugin constructor
@@ -27,7 +38,6 @@
         this.options = $.extend( {}, defaults, options) ;
         this._defaults = defaults;
         this._name = pluginName;
-        
         var container = $(element).height('auto');
         this.container = {
             element: container,
@@ -38,7 +48,6 @@
             selector: options.textSelector || ''
         };
         this.ellipsis = $('<i class="dotdotdot">&#133;</i>');
-       
         this.init();
     }
 
@@ -55,17 +64,36 @@
             }
         }
     };
-    
+
     /**
-     * Removes the spans from the text.
+     * Removes the spans from the text
      **/
-    Plugin.prototype.resetText = function () {
-        var ellipsisChar = this.ellipsis.text(),
-            txt = this.text.element.text();
-        
-        this.text.element.text(txt.replace(ellipsisChar, ellipsisChar + ' '));
+    Plugin.prototype.clean = function () {
+        var plugin = this;
+            ellipsisChar = plugin.ellipsis.text(),
+            txtElement = plugin.text.element,
+            attr = plugin.options.attributes,
+            oTxt = plugin.text.value,
+            tTxt = plugin.text.element.text();
+
+        plugin.text.truncated = tTxt.substring(0, tTxt.indexOf(ellipsisChar) + ellipsisChar.length);
+        txtElement.text(plugin.text.truncated);
+
+        if (attr) {
+            if ($.isArray(attr)) {
+                $.each(attr, function () {
+                    var at = this.toString();
+                    txtElement.attr(at, oTxt);
+                });
+            } else if (typeof attr == 'string') {
+                txtElement.attr(attr, oTxt);
+            }
+        }
     };
 
+    /**
+     * Initialize the plugin
+     **/
     Plugin.prototype.init = function () {
         var plugin = this;
         // set the text properties of the plugin instance
@@ -76,21 +104,19 @@
             plugin.text.element = $(plugin.text.selector, plugin.container.element).eq(0);
             plugin.text.value = $.trim(plugin.text.element.text());
         }
-        
+
         // process the text for truncation
         var words = this.text.value.split(' '),
             spanned = '<span style="display:inline-block">' + words.join('</span> <span style="display:inline-block">') + '</span>',
             lastWord, dotWidth, spaceWidth;
-        
         plugin.text.element.html(spanned).append(plugin.ellipsis);
         plugin._setMaxHeight();
-        
+
         // process the text and insert an ellipsis where it should be truncated
         if (plugin.container.maxHeight && plugin.text.element.height() > plugin.container.maxHeight) {
             plugin.text.element.find('span').each(function () {
                 var word = $(this),
                     pos = word.position();
-                
                 if (word.width() > plugin.container.width) {
                     word.siblings('span').hide();
                     plugin.ellipsis.css({
@@ -111,24 +137,23 @@
                         lastWord = word;
                     }
                     return false;
-                } 
+                }
                 lastWord = word;
             });
             lastWord.before(plugin.ellipsis);
-            
+
             // set the content back to text only, i.e. remove the spans surrounding each word.
-            plugin.resetText();
+            plugin.clean();
         }
     };
-    
+
     // add the plugin to jQuery
     $.fn[pluginName] = function ( options ) {
         return this.each(function () {
             if (!$.data(this, 'plugin_' + pluginName)) {
-                $.data(this, 'plugin_' + pluginName, 
+                $.data(this, 'plugin_' + pluginName,
                 new Plugin( this, options ));
             }
         });
-    }
-
+    };
 })( jQuery, window, document );
